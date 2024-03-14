c33dbff09c072e5d1e51220a64963218e0a6ecff
Orion Gunning <orion.gunning@gmail.com>
2024-03-14 16:51:38 +1100

    Update index.ts

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -76,7 +76,7 @@ class App {
         this.game = new GameController(this);
 
         // set default scene
-        this.game.setScene(SceneName.GAME);
+        this.game.setScene(SceneName.HOME);
 
         // main render loop & state machine
         await this._render();
